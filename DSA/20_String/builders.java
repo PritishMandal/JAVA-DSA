c52e040b2fc8af75f060f83d5d749d6b9d81cf42
Pritish mandal <pritishmandal21@gmail.com>
2026-09-18 13:36:34 +0530

    All files sorting

--- DSA/20_String/builders.java
+++ DSA/20_String/builders.java
@@ -4,12 +4,11 @@ public class builders {
         StringBuilder sb =new StringBuilder("");
         for(char ch='a';ch<='z';ch++){
           //sb.append(ch);
-          sb.append(ch).append(" ");
+          sb.append(ch).append(" ");// ye space bhi add kar dega har character ke baad, agar space nahi chahiye to sirf sb.append(ch) use kar sakte ho
         }
-        System.out.println(sb);
+        System.out.println(sb);//
     
     }
-
 }
 
 
